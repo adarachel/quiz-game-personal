12c27713e931ef6fbac97504c465e28443c5ba6a
adarachel <108772486+adarachel@users.noreply.github.com>
2022-12-12 15:27:04 +0100

    greys quiz questions

---
+++ js/questions.js
@@ -0,0 +1,245 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many seasons of Grey's Anatomy are there?",
+    answer: "18 and counting",
+    options: [
+      "12",
+      "18 and counting",
+      "20",
+      "15"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When did Grey's Anatomy start?",
+    answer: "2005",
+    options: [
+      "2004",
+      "2005",
+      "2006",
+      "2007"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What season is the musical episode in?",
+    answer: "Season seven",
+    options: [
+      "Season nine",
+      "Season six",
+      "Season seven",
+      "Season eight"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What color scrubs do interns wear? What about attendings?",
+    answer: "Light blue for interns, dark blue for attendings",
+    options: [
+      "Blue for interns, grey for attendings",
+      "Light blue for interns, dark blue for attendings",
+      "Light green for interns, dark green for attendings",
+      "Pink for interns, Orange for attendings"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is Derek's signature saying?",
+    answer: "It's a beautiful day to save lives. Let's have some fun",
+    options: [
+      "I'm McDreamy",
+      "This isn't right. You should have taken me to get a head CT",
+      "It's a beautiful day to save lives",
+      "It's a beautiful day to save lives. Let's have some fun"
+    ]
+  },
+    {
+    numb: 6,
+    question: "As of season 18, how many original cast members are still on the show?",
+    answer: "Three: Meredith, Bailey, Webber",
+    options: [
+      "Six: Meredith, Karev, Avery, Kepner, Bailey, Webber",
+      "Five: Meredith, Karev, Avery, Bailey, Webber",
+      "Four: Merdith, Avery, Bailey, Webber",
+      "Three: Meredith, Bailey, Webber"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Who are Meredith's first roommates?",
+    answer: "Izzie and George",
+    options: [
+      "Izzie and George",
+      "Cristina, Izzie and George",
+      "Izzie, George and Alex",
+      "Cristina, Izzie, George and Alex"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of these is not a character nickname?",
+    answer: "McDoctor",
+    options: [
+      "007",
+      "Medusa",
+      "The Nazi",
+      "McDoctor"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of these characters started out as enemies?",
+    answer: "Meredith and Alex",
+    options: [
+      "Meredith and Alex",
+      "Addison and Bailey",
+      "Webber and Ellis",
+      "George and Burke"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of these characters had the most memorable introduction?",
+    answer: "Addison Montgomery",
+    options: [
+      "Lexie Grey",
+      "Addison Montgomery",
+      "Catherine Avery",
+      "Mark Sloan"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which of these was the shortest marraige?",
+    answer: "Meredith and Alex",
+    options: [
+      "Alex and Izzie",
+      "Cristina and Owen",
+      "George and Callie",
+      "Owen and Amelia"
+    ]
+  },
+  {
+    numb: 12,
+    question: "What was Meredith's dog's name?",
+    answer: "Doc",
+    options: [
+      "Finn",
+      "Bingo",
+      "Doc",
+      "Shepherd"
+    ]
+  },
+  {
+    numb: 13,
+    question: "How many sisters does Derek have?",
+    answer: "Four",
+    options: [
+      "Four",
+      "Three",
+      "Five",
+      "Six"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which is the best hospital name?",
+    answer: "Seattle Grace Mercy Death",
+    options: [
+      "Seattle Grace Mercy West",
+      "Seattle Grace",
+      "Grey Sloan Memorial",
+      "Seattle Grace Mercy Death"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Why does Izzie see visions of Denny Duquette?",
+    answer: "She had a brain tumor",
+    options: [
+      "She felt guilty for cutting his LVAD wire",
+      "She missed him so much",
+      "Because he left her a lot of money",
+      "She had a brain tumor"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Where does Derek propose to Meredith?",
+    answer: "In the elevator",
+    options: [
+      "In the locker room with a post it",
+      "At Meredith's field of candles",
+      "At Derek's Trailer",
+      "In the elevator"
+    ]
+  },
+  {
+    numb: 17,
+    question: "What unusual color is Cristina's wedding dress in season 7?",
+    answer: "Red",
+    options: [
+      "Red",
+      "Black",
+      "Purple",
+      "Brown"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Which doctors are in the plane crash?",
+    answer: "Meredith and Alex",
+    options: [
+      "Meredith, Alex, Derek, Mark, Lexie, and Arizona",
+      "Meredith, Cristina, Derek, Mark, Lexie, and Callie",
+      "Meredith, Cristina, Mark, Lexie, and Arizona.",
+      "Meredith, Cristina, Derek, Mark, Lexie, and Arizona."
+    ]
+  },
+  {
+    numb: 19,
+    question: "Who is Ada's favorite character?",
+    answer: "Lexie Grey",
+    options: [
+      "Lexie Grey",
+      "Alex Karev",
+      "Miranda Bailey",
+      "Cristina Yang"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Which character death was the most painful?",
+    answer: "Derek Shepherd",
+    options: [
+      "Mark Sloan",
+      "George O'Malley",
+      "Lexie Grey",
+      "Derek Shepherd"
+    ]
+  },
+  {
+    numb: 21,
+    question: "Which was the best teacher-student duo?",
+    answer: "Plastics: Mark Sloan and Jackson Avery",
+    options: [
+      "Plastics: Mark Sloan and Jackson Avery",
+      "Pediatrics: Arizona Robbins and Alex Karev",
+      "Cardio: Teddy Altman and Cristina Yang",
+      "General: Richard Webber and Meredith Grey"
+    ]
+  },
+  {
+    numb: 22,
+    question: "When did Grey's become uninteresting?",
+    answer: "After Derek Shepherd died",
+    options: [
+      "After the shooting",
+      "After the plane crash",
+      "After Cristina Yang left",
+      "After Derek Shepherd died"
+    ]
+  },
+];
